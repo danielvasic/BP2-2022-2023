17106482a3225f6cfac68ef382e4a68ddf72087c
dvasi_fpmoz <dvasic1@gmail.com>
2023-06-07 11:52:14 +0200

    Final projekt

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ WORKDIR /home/app/
 COPY . /home/app/
 RUN pip install -r ./requirements.txt
 
-CMD [ "python3", "-m", "flask", "run", "--host=0.0.0.0"]
+CMD [ "python3", "app.py"]
--- app.py
+++ app.py
@@ -5,36 +5,41 @@ from flask import Flask, request, render_template
 from flask import jsonify
 import json
 from kafka import KafkaProducer, KafkaConsumer
+from flask_socketio import SocketIO, emit
+from flask_cors import CORS
+import threading
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 def json_serializer(data):
-    return jsonify(data)
+    return json.dumps(data).encode('utf-8')
 
-def json_deserializer():
-    return request.get_json()
+def json_deserializer(data):
+    return json.loads(data)
 
 producer = KafkaProducer(
     bootstrap_servers=['kafka:9092'],
     value_serializer=json_serializer
 )
 
+consumer = KafkaConsumer(
+    'razred',
+    bootstrap_servers=['kafka:9092'],
+    value_deserializer=json_deserializer,
+    group_id='test-group',
+    auto_offset_reset='earliest'
+)
+
+kafka_thread = None
+
+
 @app.route("/")
 def index ():
     classes = session.query(Razred).all()
-    consumer = KafkaConsumer(
-        'razred',
-        bootstrap_servers=['kafka:9092'],
-        value_deserializer=json_deserializer,
-        group_id='test-group',
-        auto_offset_reset='earliest'
-    )
-    def consume_classes ():
-       for message in consumer:
-            razred = message.value
-            yield razred
-
-    return stream_template('classes.html', classes=classes, stream=consume_classes())
+    return render_template('classes.html', classes=classes)
 
 @app.route("/classes/delete/<int:id_razreda>", methods=["DELETE"])
 def delete_class(id_razreda):
@@ -61,7 +66,7 @@ def get_class(id_razreda):
     if razred:
         # pretvoriti objekt Razred u rječnik
         # Uspješno dohvaćeno
-        return jsonify(vars(razred)), 200
+        return jsonify([{"ID_razreda": razred.ID_razreda, "godina": razred.godina, "odjeljenje": razred.odjeljenje}]), 200
     else:
         # Nema Razreda s ovim ID-om
         return jsonify({'message': f'Nema razreda s ID {id_razreda}.'}), 404
@@ -81,7 +86,12 @@ def edit_class():
                 razred.godina = godina
             if odjeljenje: 
                 razred.odjeljenje = odjeljenje
+            
             session.commit()
+
+            producer.send("razred", [{"ID_razreda": razred.ID_razreda, "godina": razred.godina, "odjeljenje": razred.odjeljenje}])
+            producer.flush()
+
             # Uspješno ažurirano
             return jsonify({'message': f'Razred sa ID {id_razreda} je ažuriran.'}), 200
         else:
@@ -96,22 +106,29 @@ def add_class():
     # Dohvati 'godina' i 'odjeljenje'
     godina = request.form.get("godina")
     odjeljenje = request.form.get("odjeljenje")
-
+    
     # Dodaj novi razred
     razred = Razred(godina=godina, odjeljenje=odjeljenje)
     session.add(razred)
     session.commit()
 
-    producer.send("razred", vars(razred))
+    producer.send("razred", [{"ID_razreda": razred.ID_razreda, "godina": razred.godina, "odjeljenje": razred.odjeljenje}])
     producer.flush()
 
     # Dobra je praksa vratiti ispravan JSON zahtjev
     return jsonify({'message': 'Dodan novi razred u bazu.'})
 
+@socketio.on('connect', namespace='/kafka')
+def connect():
+    global kafka_thread
+    if kafka_thread is None or not kafka_thread.is_alive():
+        kafka_thread = threading.Thread(target=kafka_consumer)
+        kafka_thread.start()
 
-def stream_template(template_name, **context):
-    """Enabling streaming back results to app"""
-    app.update_template_context(context)
-    template = app.jinja_env.get_template(template_name)
-    streaming = template.stream(context)
-    return streaming
+def kafka_consumer():
+    for poruka in consumer:
+        razred = poruka.value
+        socketio.emit('data', {'razred': razred}, namespace='/kafka')
+
+if __name__ == '__main__':
+    socketio.run(app, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
